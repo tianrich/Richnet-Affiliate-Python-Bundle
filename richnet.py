ca3dd12983a30771e8da0e2fa92a29a49e379702
tianrich <78232090+tianrich@users.noreply.github.com>
2023-01-21 21:27:25 +0000

    Update richnet.py

--- richnet.py
+++ richnet.py
@@ -1,13 +1,17 @@
+import pyperclip
+
 def convert_to_digidip(original_link):
-//change the return to add your own affiliate conversion link and delete this text
-return "https://go.skimresources.com?id=100269X1558466&xs=1&url=" + original_link
+    return "https://go.skimresources.com?id=100269X1558466&xs=1&url=" + original_link
 
 while True:
     original_link = input("Please enter a website link: ")
     digidip_link = convert_to_digidip(original_link)
     print("The digidip link is: " + digidip_link)
-    user_input = input("Enter 'r' to reset, 'e' to exit: ")
+    user_input = input("Enter 'r' to reset, 'c' to copy link, 'e' to exit: ")
     if user_input.lower() == 'e':
         break
+    elif user_input.lower() == 'c':
+        pyperclip.copy(digidip_link)
+        print("Link copied to clipboard")
     elif user_input.lower() != 'r':
         print("Invalid input")

4255192b00d30a21779df4f5a91ef721fc700af1
tianrich <78232090+tianrich@users.noreply.github.com>
2023-01-22 10:15:01 +0000

    Update richenetamazon.py

--- richenetamazon.py
+++ richenetamazon.py
@@ -1,12 +1,36 @@
-def convert_to_digidip(original_link):
-    return original_link + "richnet-20" 
+import pyperclip
+import tweepy
+
+# Twitter API credentials
+consumer_key = 'YOUR_CONSUMER_KEY'
+consumer_secret = 'YOUR_CONSUMER_SECRET'
+access_token = 'YOUR_ACCESS_TOKEN'
+access_secret = 'YOUR_ACCESS_SECRET'
+
+# Authenticate with Twitter API
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+api = tweepy.API(auth)
 
 while True:
-    original_link = input("Please enter a website link: ")
-    digidip_link = convert_to_digidip(original_link)
-    print("The digidip link is: " + digidip_link)
-    user_input = input("Enter 'r' to reset, 'e' to exit: ")
-    if user_input.lower() == 'e':
-        break
-    elif user_input.lower() != 'r':
-        print("Invalid input")
+    # Get input from user
+    amazon_link = input("Enter an Amazon link: ")
+    item_name = input("Enter the name of the item: ")
+    item_price = input("Enter the price of the item: ")
+
+    # Add affiliate link
+    affiliate_link = "spottedbargains-21"
+    final_link = amazon_link + "?tag=" + affiliate_link
+
+    # Share link on Twitter with item name, price and #ad
+    message = "Check out this great deal on " + item_name + " for only $" + item_price + "! " + final_link + " #ad"
+    api.update_status(message)
+    print("Link shared on Twitter!")
+
+    # Give option to copy link
+    copy_choice = input("Do you want to copy the link? (yes/no): ")
+    if copy_choice == "yes":
+        pyperclip.copy(final_link)
+        print("Link copied to clipboard!")
+    else:
+        print("Link not copied.")

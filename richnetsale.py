7648d6e277a312425339d9f3b63bf6ed8d3a0e17
Tian R <78232090+tianrich@users.noreply.github.com>
2023-01-22 12:07:14 +0000

    Update richnetsale.py

--- richnetsale.py
+++ richnetsale.py
@@ -5,7 +5,6 @@ consumer_key = ''
 consumer_secret = ''
 access_token = ''
 access_token_secret = ''
-
 # Connect to the Twitter API using Tweepy
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -13,8 +12,9 @@ api = tweepy.API(auth)
 
 while True:
     shop_name = input("Enter the shop name: ")
-    link = input("Enter a link to share: ")
-    affiliated_link = f"******************{link}"
-    message = f"Spotted online - {shop_name} has a sale on at the moment #ad #richnet {affiliated_link}"
+    url_link = input("Enter a link to share: ")
+    additional_text = input("Enter additional text to share: ")
+    affiliated_link = f"https://go.skimresources.com?id=100269X1558466&xs=1&url="
+    message = f"Spotted online - {shop_name} has a sale on at the moment {additional_text} #ad #spotted {affiliated_link}{url_link}"
     api.update_status(message)
     print("Shared to Twitter: ", message)

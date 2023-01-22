4ee6016e2f474904fb19dba0ed7fc242c1c709eb
Tian R <78232090+tianrich@users.noreply.github.com>
2023-01-22 11:47:28 +0000

    Create richnetsale.py

---
+++ richnetsale.py
@@ -0,0 +1,20 @@
+import tweepy
+
+# Twitter API credentials
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+
+# Connect to the Twitter API using Tweepy
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+while True:
+    shop_name = input("Enter the shop name: ")
+    link = input("Enter a link to share: ")
+    affiliated_link = f"https://spottedbargains.digidip.net/visit?url={link}"
+    message = f"Spotted online - {shop_name} has a sale on at the moment #ad #richnet {affiliated_link}"
+    api.update_status(message)
+    print("Shared to Twitter: ", message)

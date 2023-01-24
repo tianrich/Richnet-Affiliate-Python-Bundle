b5ec245e5c4344cb89752a18f504354d3f643b26
Tian R <78232090+tianrich@users.noreply.github.com>
2023-01-24 19:06:24 +0000

    Update richitem.py

--- richitem.py
+++ richitem.py
@@ -6,21 +6,30 @@ consumer_secret = ''
 access_token = ''
 access_token_secret = ''
 
-# Authenticate with Twitter API
+# Authenticate with Twitter
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 while True:
-    # Get product information from user input
-    product_title = input("Enter product title: ")
-    old_price = input("Enter old price: ")
-    deal_price = input("Enter deal price: ")
-    url = input("Enter product URL: ")
-    shop_name = input("Enter shop name: ")
-    # Create tweet text
-    tweet = f"Check out this deal from {shop_name}! {product_title} is on sale for £{deal_price} (was £{old_price}). #ad #spotted >> {url}"
-    # Print tweet text
-    print(tweet)
-    # Share tweet
-    api.update_status(tweet)
+    # item information
+    item_name = input("Enter the item name: ")
+    price_before = input("Enter the price before the deal: ")
+    deal_price = input("Enter the deal price: ")
+    product_link = input("Enter the product link: ")
+    affiliate_link = getpass.getpass("Enter your affiliate link: ") # hide the input from the terminal
+    use_voucher_code = input("Do you want to use a voucher code? (yes/no): ")
+    if use_voucher_code == 'yes':
+        voucher_code = input("Enter the voucher code: ")
+    else:
+        voucher_code = None
+
+    # tweet message
+    message = f'{item_name} on sale! Before: {price_before}, Now: {deal_price}'
+    if voucher_code:
+        message += f' Use code {voucher_code} at checkout! '
+    message += f'Link: {affiliate_link}{product_link}'
+
+    # post the tweet
+    tweet = api.update_status(message)
+    print(f'Tweet sent! Tweet id: {tweet.id}')

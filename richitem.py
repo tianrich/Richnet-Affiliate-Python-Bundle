5562c77978b9995fefdf6df8de9ca66cc4a9559e
Tian R <78232090+tianrich@users.noreply.github.com>
2023-01-24 19:08:37 +0000

    Update richitem.py

--- richitem.py
+++ richitem.py
@@ -1,12 +1,14 @@
 import tweepy
+import getpass
+import time
 
-# Twitter API credentials
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_token_secret = ''
+# enter your Twitter API keys
+consumer_key = input("Enter your consumer key: ")
+consumer_secret = input("Enter your consumer secret: ")
+access_token = input("Enter your access token: ")
+access_token_secret = input("Enter your access token secret: ")
 
-# Authenticate with Twitter
+# connect to the Twitter API
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
@@ -17,7 +19,11 @@ while True:
     price_before = input("Enter the price before the deal: ")
     deal_price = input("Enter the deal price: ")
     product_link = input("Enter the product link: ")
-    affiliate_link = getpass.getpass("Enter your affiliate link: ") # hide the input from the terminal
+    use_affiliate_link = input("Do you want to use an affiliate link? (yes/no): ")
+    if use_affiliate_link == 'yes':
+        affiliate_link = getpass.getpass("Enter your affiliate link: ") # hide the input from the terminal
+    else:
+        affiliate_link = ""
     use_voucher_code = input("Do you want to use a voucher code? (yes/no): ")
     if use_voucher_code == 'yes':
         voucher_code = input("Enter the voucher code: ")
@@ -33,3 +39,6 @@ while True:
     # post the tweet
     tweet = api.update_status(message)
     print(f'Tweet sent! Tweet id: {tweet.id}')
+
+    # wait for a certain amount of time before posting the next tweet
+    time.sleep(3600) # waits for 1 hour before posting the next tweet
